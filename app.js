bcaf3a585d7b8970d3e67469da9e4d34ffb95110
Goncharuk <OlyaGoncharuk@yandex.ru>
2021-07-15 22:27:11 +0300

    signup правки

--- app.js
+++ app.js
@@ -70,10 +70,6 @@ app.post(
       email: Joi.string().required().email(),
       password: Joi.string().required().min(8),
       name: Joi.string().min(2).max(30),
-      about: Joi.string().min(2).max(30),
-      avatar: Joi.string().regex(
-        /^(https?:\/\/)([\da-z.-]+)\.([a-z.]{2,6})([/\w\W.-]*)#?$/,
-      ),
     }),
   }),
   createUser,
